9a7a4d0beb848fdebbc70324fa64918ca1d26252
Rachid Bouain <rachidbouain@MacBook-Air-de-Rachid.local>
2019-12-24 10:43:09 +0100

    Remove unnecessary console.log call

--- public/script/app.js
+++ public/script/app.js
@@ -1,5 +1,4 @@
 
-console.log("Client Side Javascript File")
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
