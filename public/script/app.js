50d729a3fb7745397b74b7979699d2ad009930af
bouainrachid <rachidbouain.mba@gmail.com>
2019-12-25 15:31:50 +0100

    Setup app for Heroku

--- public/script/app.js
+++ public/script/app.js
@@ -17,7 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
       console.log(data)
     })
   })
-  fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+  fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
