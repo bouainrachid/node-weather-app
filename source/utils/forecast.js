c207c2ed9e527550dc82981029153521947253be
bouainrachid <rachidbouain.mba@gmail.com>
2019-12-26 12:08:54 +0100

    Add high and low to forecaste

--- source/utils/forecast.js
+++ source/utils/forecast.js
@@ -26,10 +26,11 @@ request({ url : url, json:true}, (error, { body }) => {
     } else if (body.error) {
         callback('Unable to find location',undefined)
     } else {
-        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + " degress out. " + " There is a " + body.currently.precipProbability + ' chance of rain.')
+        callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + " degress out. This high today is " + body.daily.data[0].temperatureHigh + " with a low of " + body.daily.data[0].temperatureLow + ". There is a " + body.currently.precipProbability + ' chance of rain.')
     }
 })
 
 }
 
 module.exports = forecast
+
